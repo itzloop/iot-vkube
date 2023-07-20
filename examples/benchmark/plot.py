7fcc28f2222d551893d82405ec52133ef02c4a47
itzloop <sina.sh.k78@gmail.com>
2023-07-20 17:19:28 +0330

    Adds slides

--- examples/benchmark/plot.py
+++ examples/benchmark/plot.py
@@ -8,27 +8,37 @@ def make_farsi_text(x):
     farsi_text = get_display(reshaped_text)
     return farsi_text
 
-plt.rcParams.update({'font.size': 16})
+TEXT = '#e5e5e5'
+MAIN = '#fca311'
+plt.rcParams.update({
+    'font.size': 8,
+    'text.color': TEXT,
+    'axes.labelcolor': TEXT,
+    'axes.edgecolor': MAIN,
+    'xtick.color': TEXT,
+    'ytick.color': TEXT,
+})
+
 
 # Read the CSV file
-data = pd.read_csv('output.csv')
-
+data = pd.read_csv('output2.csv')
 
 fig, ax = plt.subplots(1)
 
-xs = data.Time.values
-ys = data.DevicePerController.values
-ax.set_xlabel(make_farsi_text("مدت زمان"))
-ax.set_ylabel(make_farsi_text("تعداد دستگاه به ازای یک کنترل‌کننده"))
-ax.set_title(make_farsi_text("تعداد دستگاه به ازای یک کنترل‌کننده به مدت زمان"))
-
 # xs = data.Time.values
-# ys = data.Controllers.values
-# ax.set_xlabel(make_farsi_text("مدت زمان"))
-# ax.set_ylabel(make_farsi_text("تعداد کنترل‌کننده"))
-# ax.set_title(make_farsi_text("تعداد کنترل‌کننده به مدت زمان"))
-ax.plot(xs, ys, marker="o")
+# ys = data.DevicePerController.values
+# ax.set_xlabel(make_farsi_text("Duration"))
+# ax.set_ylabel(make_farsi_text("IoT Device Count For One Controller"))
+# ax.set_title(make_farsi_text("IoT Device Count For One Controller To Duration"))
+
+xs = data.Time.values
+ys = data.Controllers.values
+ax.set_xlabel(make_farsi_text("Duration"))
+ax.set_ylabel(make_farsi_text("Controller Count"))
+ax.set_title(make_farsi_text("Controllers Count To Durations"))
+ax.plot(xs, ys, marker="o", color="#ffffff")
 fig.autofmt_xdate(rotation=45)
+plt.savefig('controllers_performance_benchmark.png', format='png', dpi=float(600), transparent=True)
 plt.show()
 
 
